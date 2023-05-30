62e77627b9ced4030c14de626e597953268a9428
Kerim <kerimberdi.agayev@edusoftech.eu>
2023-05-30 14:13:15 +0300

    api edit

--- pages/api/apply.ts
+++ pages/api/apply.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next"
+import { MongoClient, ServerApiVersion } from "mongodb"
 
-const { MongoClient, ServerApiVersion } = require("mongodb")
 require("dotenv").config()
 
-const uri = process.env.MONGODB_URI
+const uri = process.env.MONGODB_URI!
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
