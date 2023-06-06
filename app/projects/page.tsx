e97145360d99c609fdb83febf46eab014f8cf27b
joaovwfreire <jvwfreire@gmail.com>
2023-06-06 06:30:27 -0300

    project card component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+import * as React from "react"
 import Image from "next/image"
 import Link from "next/link"
 import { Clock, DollarSign, Info, Leaf, Users } from "lucide-react"
@@ -13,6 +15,8 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Progress } from "@/components/ui/progress"
+
+import { NextApiRequest, NextApiResponse } from "next"
 
 type OngoingProjects = {
   id: string
@@ -30,6 +34,7 @@ type OngoingProjects = {
   saleStartDate: Date
   saleEndDate: Date
   nct: boolean
+  contractAddress?: string
 }
 
 const ongoingProjects: OngoingProjects[] = [
@@ -101,8 +106,119 @@ const notifications = [
 ]
 
 const ProjectsPage = () => {
+  const [projects, setProjects] = React.useState<OngoingProjects[]>([])
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError]: any = React.useState("")
+
+  React.useEffect(() => {
+    setLoading(true)
+    async function getData() {
+      try {
+        
+        const projects = fetch("http://localhost:3000/api/projects")
+        .then((res) => res.json())
+        .then((data) => {
+          setProjects(data.data)
+        })
+        return { projects };
+      } catch (err) {
+        setError(err)
+      } 
+    }
+ 
+    getData()
+  }, [])
+
   return (
     <section className="container grid items-center gap-6 pb-8 pt-6 md:grid-cols-2 md:py-10 xl:grid-cols-3">
+      {projects.map((project, index) => (
+        <Card
+          className={cn(
+            `w-[380px] ${project.nct && "bg-[#ecec7b] dark:bg-[#0A1F04]"}`
+          )}
+          key={project.id}
+        >
+          <CardHeader>
+            <div className="flex items-center space-x-2">
+              <Image
+                src="https://dummyimage.com/50x50"
+                alt=""
+                width={50}
+                height={50}
+              />
+              <div>
+                <CardTitle>{project.name}</CardTitle>
+                <CardDescription>{project.description}</CardDescription>
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div>
+              <div className="mb-4 grid grid-cols-[25px_1fr] items-start gap-2 pb-4 last:mb-0 last:pb-0">
+                <Leaf className="flex translate-y-1 text-[#23e7c3]" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">Round Name</p>
+                  <p className="text-sm text-muted-foreground">
+                    {project.roundName}
+                  </p>
+                </div>
+              </div>
+              <div className="mb-4 grid grid-cols-[25px_1fr] items-start gap-2 pb-4 last:mb-0 last:pb-0">
+                <Users className="flex translate-y-1 text-[#23e7c3]" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    Participants
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {project.participants}
+                  </p>
+                </div>
+              </div>
+              <div className="mb-4 grid grid-cols-[25px_1fr] items-start gap-2 pb-4 last:mb-0 last:pb-0">
+                <Clock className="flex translate-y-1 text-[#23e7c3]" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">Sale End</p>
+                  <p className="text-sm text-muted-foreground">
+                    {project.saleStartDate.toLocaleString("en-GB", {
+                      month: "2-digit",
+                      day: "2-digit",
+                      year: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                      timeZone: "UTC",
+                    })}
+                  </p>
+                </div>
+              </div>
+              <div className="mb-4 grid grid-cols-[25px_1fr] items-start gap-2 pb-4 last:mb-0 last:pb-0">
+                <DollarSign className="flex translate-y-1 text-[#23e7c3]" />
+                <div className="space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    Raised Amount
+                  </p>
+                  <p className="text-sm text-muted-foreground">
+                    {project.raisedAmount.currentAmount} /{" "}
+                    {project.raisedAmount.targetAmount}{" "}
+                    {project.raisedAmount.currency}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <Progress value={33} />
+          </CardContent>
+          <CardFooter>
+            <Link
+              className={buttonVariants({
+                variant: "default",
+                className: "w-full",
+              })}
+              href={`/projects/${project.id}`}
+            >
+              <Info className="mr-2 h-4 w-4" /> View Details
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
       {ongoingProjects.map((project, index) => (
         <Card
           className={cn(
---
+++ pages/api/projects.ts
@@ -0,0 +1,60 @@
+import { NextApiRequest, NextApiResponse } from "next"
+
+const { MongoClient, ServerApiVersion } = require("mongodb")
+require("dotenv").config()
+
+const uri = process.env.MONGODB_URI
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+})
+
+interface ProjectData {
+  fullName: string
+  email: string
+  title: string
+  telegramUser: string
+  projectName: string
+  projectDescription: string
+  tokenInfo: string
+  targetRaise: string
+  chain: string
+  projectStatus: string
+  raisedBefore: string
+  projectType: string
+  websiteUrl: string
+  whitepaperUrl: string | null
+  projectTwitter: string | null
+  projectTelegram: string | null
+  projectGithub: string | null
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<any>
+) {
+  const body: ProjectData = req.body
+
+  //run().catch(console.dir);
+  // we will use params to access the data passed to the dynamic route
+  if (req.method === "GET") {
+    // insert into db
+    try {
+      await client.connect()
+      const database = client.db("ecoseeds")
+      const collection = database.collection("projects")
+      const result = await collection.find({}).toArray()
+      res.status(200).json({ success: true, data: result })
+    } catch (err) {
+      console.log(1)
+      res.status(400).json({ success: false })
+    } finally {
+      await client.close()
+    }
+  }
+}
